870669e6acc37cc91ddacf6c247aac2084873ee6
Jonathan Smith <jonathan@impacti.org>
2012-03-03 11:17:02 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hq::Application.config.secret_token = '0ce6a1a3fc295194ac4a6af299d158862a02200eb76ca19dd8ffde549a0fe21f94c9986c32c12ec548aee497cb94bef143a928f8b9637375a3e5bba90039257c'
