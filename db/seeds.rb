e2e24c9414e331e9b5e4850bdc00226fc2569680
Jonathan Smith <jonathan@impacti.org>
2012-03-05 20:09:49 +0000

    cleaned a bit more

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 species = Species.create([{common_name: 'lion', binomial: "panthera leo"}, {common_name: 'leopard', binomial: "panthera pardus"}])
-Sighting.create(species_id: 1, tribe_id: 1, location_id: 1, drive_id: 1, description: "Something in a tree - was really dangerous", user_id: 1, submission_point: "HQ", record_time: "2012-03-05 19:42:59", time_window_hrs: 2)     
+Sighting.create(species_id: 1, tribe_id: 1, location_id: 1, drive_id: 1, description: "Something in a tree - was really dangerous", user_id: 1, submission_point: "HQ", record_time: "2012-03-05 19:42:59", time_window_hr: 2)     
 Tribe.create(name: 'Duma pride', location_id: 2, species_id: 1)
 locations = Location.create([{name: 'End of the garden'},{name: 'Other side of the wall'}])
 Drive.create(duration_hrs: 3, distance_km: 15, description: 'Round and round')
