3cd1e98a2088e289f0a07aba9caf8d4bd779ec7a
Jonathan Smith <jonathan@impacti.org>
2012-03-07 20:51:51 +0000

    moved entry table to partial - for ajax

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,6 +4,18 @@ class SightingsController < ApplicationController
     @camp = Camp.first
     @drive_count = Drive.count
     @sightings = Sighting.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @sightings }
+    end
+  end
+
+  def show
+    @sighting = Sighting.find(params[:id])
+    respond_to do |format|
+      format.html #show.html.erb
+      format.json { render json: @sighting}
+    end
   end
 
   def new
