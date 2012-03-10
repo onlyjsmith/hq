8707e0a025b74b748f3483b1475c4bccc09f5f66
Jonathan Smith <jonathan@impacti.org>
2012-03-10 19:35:20 +0000

    added railsadmin, devise, user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Hq::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  devise_for :users
+
   resources :sightings
   resources :locations
   # get "sightings/index"
