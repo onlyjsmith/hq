04ebe81a805f6b2beddbcb81d91d1ecb423f666c
Jonathan Smith <jonathan@impacti.org>
2012-03-10 07:52:28 +0000

    test adding simple form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,12 @@ class LocationsController < ApplicationController
   
   def index
     @locations = Location.search(params[:search])
+    @search = Location.new
+    # puts "SEARCH TERMZ: #{@search.class}"
   end
   
   def new
     @location = Location.new
   end
+  
 end
