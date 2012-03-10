49fc8d02183d94b85a11d1fb90121a2a91087db1
Jonathan Smith <jonathan@impacti.org>
2012-03-10 15:11:04 +0000

    edit show working too

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,4 +11,37 @@ class LocationsController < ApplicationController
     @location = Location.new
   end
   
+  def create
+    @location = Location.new(params[:location])
+    
+    respond_to do |wants|
+      if @location.save
+        flash[:notice] = 'Location was successfully created.'
+        wants.html { redirect_to(@location) }
+      else
+        wants.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def show
+    @location = Location.find(params[:id])
+  end
+  
+  def edit
+    @location = Location.find(params[:id])
+  end
+  
+  def update
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      if @location.update_attributes(params[:location])
+        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+  
 end
