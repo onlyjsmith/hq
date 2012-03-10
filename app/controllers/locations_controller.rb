f2f7916d88664d41718eddafaa17b4356508dc85
Jonathan Smith <jonathan@impacti.org>
2012-03-10 14:38:55 +0000

    basic index page with add / edit links

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,7 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.search(params[:search])
     @search = Location.new
+    @camp = Camp.first
     # puts "SEARCH TERMZ: #{@search.class}"
   end
   
