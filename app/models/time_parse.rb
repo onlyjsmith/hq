aabf2f1751340d19dad9efbb0847a16e36d88a01
Jonathan Smith <jonathan@impacti.org>
2012-03-11 18:08:43 +0000

    improved time parsing

--- app/models/time_parse.rb
+++ app/models/time_parse.rb
@@ -1,14 +1,22 @@
 class TimeParse
   def self.parse(input)
-    abbr = {'wk'=>'week','wks' => 'week','w' => 'week','h' => 'hour','hr' => 'hour','hrs' => 'hour','d' => 'day', 'td' => 'today', 'yest' => 'yesterday', 'yes' => 'yesterday', 'ystr' => 'yesterday'}
-    # Checks for '2d' or similar
-    input_scan = input.scan(/[0-9]+[d,w,h,m,y]/)
-    unless input_scan.count == 0
-      # swaps '2d' for '2 d'
-      input_scan = input.sub(input_scan[0], (input_scan[0].scan(/\d+|[a-zA-Z]+/).join(" ")))
+    if input
+      abbr = {'wk'=>'week','wks' => 'week','w' => 'week','h' => 'hour','hr' => 'hour','hrs' => 'hour','d' => 'day', 'td' => 'today', 'yest' => 'yesterday', 'yes' => 'yesterday', 'ystr' => 'yesterday'}
+      # Checks for '2d' or similar
+      input_scan = input.scan(/[0-9]+[d,w,h,m,y]/)
+      unless input_scan.count == 0
+        # swaps '2d' for '2 d'
+        input_scan = input.sub(input_scan[0], (input_scan[0].scan(/\d+|[a-zA-Z]+/).join(" ")))
+      end
+      # Expands abbreviations
+      input_scan = input_scan.split(' ').map{|x| abbr[x].nil? ? x : abbr[x]}.join(' ')
+      
+      parsed = Chronic.parse input_scan
+
+      return "Input of #{input} --> #{parsed}"
+      
+    else
+      "No input yet"
     end
-    # Expands abbreviations
-    input_scan = input_scan.split(' ').map{|x| abbr[x].nil? ? x : abbr[x]}.join(' ')
-    "input: '#{input}' -> #{Chronic.parse input_scan}" 
   end
 end
