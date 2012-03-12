f7de659bba25fc9f8fd056fa7421447074126aef
Jonathan Smith <jonathan@impacti.org>
2012-03-12 23:09:16 +0000

    bit messy but brings in at least one photo - live search each time

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,30 @@
 class Species < ActiveRecord::Base
   has_many :sightings
   has_many :tribes
+  
+  def first_photo_url
+    # TODO - Probably want to do this as a task and populate a DB instead, but this works!
+    FlickRaw.api_key = APP_CONFIG['FlickRaw_api_key']
+    FlickRaw.shared_secret = APP_CONFIG['FlickRaw_shared_secret']
+    # debugger
+    # puts "self #{self.common_name}"
+    # 
+    # puts "Flickr shared secret"
+    # puts APP_CONFIG['FlickRaw_shared_secret']
+    photo_url = ""
+    results = flickr.photos.search(:text => common_name, 
+      :license => '1,2,3,4,5,6,7', 
+      :privacy_filter => "1", 
+      :sort => "interestingness-desc",
+      :per_page => 10, 
+      :extras => "url_s")
+    results.each do |photo|
+      # debugger
+      if photo.respond_to?(:url_s) && photo.url_s
+        photo_url = photo.url_s
+        break
+      end
+    end
+    photo_url
+  end
 end
---
+++ config/initializers/load_config.rb
@@ -0,0 +1,2 @@
+# TODO - Check if this works in production
+APP_CONFIG = YAML.load_file(Rails.root.join("config/config.yml"))
