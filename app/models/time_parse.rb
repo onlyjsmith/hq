7d90faba111c90e0742c88a320b8309ad9e79f08
Jonathan Smith <jonathan@impacti.org>
2012-03-12 21:01:31 +0000

    time parser fixed

--- app/models/time_parse.rb
+++ app/models/time_parse.rb
@@ -1,19 +1,22 @@
 class TimeParse
   def self.parse(input)
-    if input
+    unless input.blank?
       abbr = {'wk'=>'week','wks' => 'week','w' => 'week','h' => 'hour','hr' => 'hour','hrs' => 'hour','d' => 'day', 'td' => 'today', 'yest' => 'yesterday', 'yes' => 'yesterday', 'ystr' => 'yesterday'}
       # Checks for '2d' or similar
       input_scan = input.scan(/[0-9]+[d,w,h,m,y]/)
-      unless input_scan.count == 0
+      if input_scan.count == 0
+        input_scan = input
+      else
         # swaps '2d' for '2 d'
         input_scan = input.sub(input_scan[0], (input_scan[0].scan(/\d+|[a-zA-Z]+/).join(" ")))
       end
-      # Expands abbreviations
+
+      # expands abbreviations
       input_scan = input_scan.split(' ').map{|x| abbr[x].nil? ? x : abbr[x]}.join(' ')
       
       parsed = Chronic.parse input_scan
 
-      return "Input of #{input} --> #{parsed}"
+      return "Input of #{input} --> converted to #{parsed}"
       
     else
       "No input yet"
