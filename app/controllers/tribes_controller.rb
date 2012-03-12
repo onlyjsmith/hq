2f2bedeb1649c25625a5acdd2c982e97e894576a
Jonathan Smith <jonathan@impacti.org>
2012-03-12 21:45:58 +0000

    associations and methods done too

--- app/controllers/tribes_controller.rb
+++ app/controllers/tribes_controller.rb
@@ -16,9 +16,31 @@ class TribesController < ApplicationController
   end
 
   def create
+    @tribe = Tribe.new(params[:tribe])
+  
+    respond_to do |wants|
+      if @tribe.save
+        flash[:notice] = 'Tribe was successfully created.'
+        wants.html { redirect_to(@tribe) }
+        wants.xml { render :xml => @tribe, :status => :created, :location => @tribe }
+      else
+        wants.html { render :action => "new" }
+        wants.xml { render :xml => @tribe.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
+
   def update
+    @tribe = Tribe.find(params[:id])
+
+    respond_to do |format|
+      if @tribe.update_attributes(params[:tribe])
+        format.html { redirect_to @tribe, notice: 'Tribe was successfully updated.' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
   end
 
   def destroy
