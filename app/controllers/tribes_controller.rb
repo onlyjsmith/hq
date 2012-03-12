7d298503480e8bff402b41469e9299a109c89121
Jonathan Smith <jonathan@impacti.org>
2012-03-12 21:22:31 +0000

    added few actions

--- app/controllers/tribes_controller.rb
+++ app/controllers/tribes_controller.rb
@@ -1,14 +1,18 @@
 class TribesController < ApplicationController
   def index
+    @tribes = Tribe.all
   end
 
   def show
+    @tribe = Tribe.find(params[:id])
   end
 
   def edit
+    @tribe = Tribe.find(params[:id])
   end
 
   def new
+    @tribe = Tribe.new
   end
 
   def create
