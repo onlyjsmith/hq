8e95a53b0bd1641c18d9a4b2a338934b8328b13e
Jonathan Smith <jonathan@impacti.org>
2012-03-15 07:54:04 +0000

    added 'get_better_photo' - useful for testing

--- app/models/species.rb
+++ app/models/species.rb
@@ -4,7 +4,6 @@ class Species < ActiveRecord::Base
   has_many :photos, :as => :imageable
   
   def get_photo_url
-    # debugger
     unless self.photos.blank?
       return self.photos.first.url
     end
@@ -20,14 +19,32 @@ class Species < ActiveRecord::Base
       :extras => "url_s")
     results.each do |result|
       if result.respond_to?(:url_s) && result.url_s
-        # debugger
         photo = self.photos.create(:url => result.url_s)
-        # self.photo_id = result.id
-        # self.save
-        puts photo
         break
       end
     end
     return self.photos.first.url
+  end
+  
+  def get_better_photo
+    break_count = rand(15)
+    FlickRaw.api_key = APP_CONFIG['FlickRaw_api_key']
+    FlickRaw.shared_secret = APP_CONFIG['FlickRaw_shared_secret']
+
+    results = flickr.photos.search(:text => common_name, 
+      :license => '1,2,3,4,5,6,7', 
+      :privacy_filter => "1", 
+      :sort => "interestingness-desc",
+      :per_page => 15, 
+      :extras => "url_s")
+    results.each_with_index do |result,index|
+      if result.respond_to?(:url_s) && result.url_s
+        @photo_url = result.url_s
+        break if index == break_count
+      end
+    end
+    self.photos.destroy_all
+    self.photos.create(:url => @photo_url)
+    puts "Replaced photo for #{self.common_name}"
   end
 end
