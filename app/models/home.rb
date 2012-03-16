e35f77e1b2b04eaa0677c43b704496208fad5ad0
Jonathan Smith <jonathan@impacti.org>
2012-03-16 07:57:27 +0000

    better, still not working

--- app/models/home.rb
+++ app/models/home.rb
@@ -2,7 +2,7 @@ class Home
 # Made this class to test out the multi-model autocompleter
 
   def self.search(term)
-    @model_names = {:species => :common_name,:tribe => :name,:location => :name}
+    @model_names = {'species' => 'common_name','tribe' => 'name','location' => 'name',}
     response = []
     @model_names.each do |model_name, column|
       eval("#{model_name.capitalize}").where("#{column} ILIKE ?","%#{term}%").each do |result|
