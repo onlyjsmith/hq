c51952e4b50761ccbd01b5fe37db3bcee16a5718
Jonathan Smith <jonathan@impacti.org>
2012-03-24 12:56:08 +0000

    added extra 'year' sightings filter

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -7,6 +7,7 @@ class Sighting < ActiveRecord::Base
   belongs_to :camp
   
   def self.filter_time(filter_time)
+    # debugger
     if filter_time
       where("record_time > ?", (Date.today - filter_time.to_i))
     else
