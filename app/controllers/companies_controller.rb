c66d3ce35c080c9ae4479d9da4dcc54b0ec15f8f
Jonathan Smith <jonathan@impacti.org>
2012-03-24 12:36:00 +0000

    is correctly scoping companies and camps

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,6 +7,8 @@ class CompaniesController < ApplicationController
   def camps_index
     # debugger
     session[:company_id] = params[:id]
+    # debugger
+    session[:camp_id] = nil
     redirect_to camps_path
   end
 end
