ed95584f1db0a44623eed990dd5b2f57cdea511f
Jonathan Smith <jonathan@impacti.org>
2012-03-24 15:06:50 +0000

    stopped using sessions for everything and making more restful. not finished

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,13 +1,14 @@
 class CampsController < ApplicationController
   def index
-    debugger
+    @camps = Camp.scoped
+    # debugger
     # TODO: Redirect to "no company page" as fallback instead of selecting the first company
-    @company = Company.find(session[:company_id]) || Company.first
+    # @company = Company.find(session[:company_id]) || Company.first
 
     # TODO: Scope this to only the company's camps - this doesn't feel like the right way, but it works
-    @camps = Camp.company_camps(@company.id)
+    # @camps = Camp.company_camps(@company.id)
 
-    @sightings = @camps.all.map{|e| e.sightings}.flatten
+    # @sightings = @camps.all.map{|e| e.sightings}.flatten
     
     # TODO: Calculate the interesting stats for the company
     # @most_sightings_camp = @camps.
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -2,6 +2,8 @@ class Camp < ActiveRecord::Base
   belongs_to :company
   belongs_to :location
   has_many :sightings
+  
+  default_scope where(:company_id => session[:company_id])
       
   # def self.company_camps(company_id)
   #   Camp.where(:company_id => company_id)
