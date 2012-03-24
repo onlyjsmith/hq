023b53b9d980a209f4f8434910b00802514066d1
Jonathan Smith <jonathan@impacti.org>
2012-03-24 17:28:49 +0000

    other things too but cut out charts

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,22 +3,16 @@ class SightingsController < ApplicationController
   
   def index
     @user = User.first
-    # debugger
-    # TODO: Stop this selecting first camp as a fallback
-    @camp = Camp.find(params[:camp_id])
 
-    # TODO: Scope this by Camp
-    @sightings = Sighting.filter_time(params[:filter_time])
+    @camp = Camp.find(params[:camp_id])    if params[:camp_id]         
+
+    @sightings = Sighting.scoped
+    @sightings = @sightings.by_camp(@camp) if @camp
+    @sightings = @sightings.filter_time(params[:filter_time])
+
+
     @drive_count = @sightings.map{|x| x.drive}.uniq.count
     @species = @sightings.map{|x| x.species}.uniq
-    @h = LazyHighCharts::HighChart.new('graph') do |f|
-      f.options[:chart] = {:height => 200, :width => 400, :defaultSeriesType =>  "area" }
-      f.options[:yAxis][:max] = 20
-      f.options[:title][:text] = nil 
-      f.options[:legend] = {:floating => true, :verticalAlign => 'top'}
-      f.series(:name=>'Lion', :color => '#b1ad73', :data=>[3, 20, 3, 5, 4, 10, 12, 5,6,7,7,10,9,9])
-      f.series(:name=>'Tiger', :color => '#6b8665', :data=> [1, 3, 4, 3, 3, 5, 4,7,8,8,9,9,0,0,9] )
-    end
     
     respond_to do |format|
       format.html # index.html.erb
