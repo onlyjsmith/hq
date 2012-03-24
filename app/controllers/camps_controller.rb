764980bdda505351dfbb588901b157031e4ff205
Jonathan Smith <jonathan@impacti.org>
2012-03-24 11:31:05 +0000

    scoping and space/TODO for more details added

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -5,9 +5,13 @@ class CampsController < ApplicationController
     # TODO: Stop this selecting only the first company!
     @company = session[:company] || Company.first
 
-    @camps = Camp.all
+    # TODO: Scope this to only the company's camps - this doesn't feel like the right way, but it works
+    @camps = Camp.company_camps(@company.id)
 
     @sightings = Camp.all.map{|e| e.sightings}.flatten
+    
+    # TODO: Calculate the interesting stats for the company
+    # @most_sightings_camp = @camps.
   end
           
   # TODO: Replace this with Camp HEADLINES - for now redirects to sightings#index 
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -2,6 +2,8 @@ class Camp < ActiveRecord::Base
   belongs_to :company
   belongs_to :location
   has_many :sightings
-  
-  
+      
+  def self.company_camps(company_id)
+    Camp.where(:company_id => company_id)
+  end
 end
