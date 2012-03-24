65843dd434d36e4da442b011725ef0810c80f1f9
Jonathan Smith <jonathan@impacti.org>
2012-03-24 10:17:06 +0000

    basic dynamic headline company stats

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -3,5 +3,6 @@ class CampsController < ApplicationController
     # debugger
     @company = Company.find(params[:company_id])
     @camps = Camp.all
+    @sightings = @company.camps.map{|e| e.sightings}.flatten
   end
 end
