b90f4d7bf2c6f605d8b2d82283acc401f2ac3db6
Jonathan Smith <jonathan@impacti.org>
2012-03-31 10:39:05 +0100

    even sightings working simply

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,7 +1,7 @@
 class Sighting < ActiveRecord::Base
   belongs_to :species
   belongs_to :tribe
-  # belongs_to :user
+  belongs_to :user
   belongs_to :drive
   belongs_to :location
   belongs_to :camp
