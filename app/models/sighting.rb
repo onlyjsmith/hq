5007d4d7481009158f79466139c58113051863b6
Jonathan Smith <jonathan@impacti.org>
2012-04-01 18:48:03 +0100

    validations and defaults for sightings

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -5,50 +5,21 @@ class Sighting < ActiveRecord::Base
   belongs_to :drive
   belongs_to :location
   belongs_to :camp
+
+  validates :species_id, :presence => true
+  validates :location_id, :presence => true
+  validates :description, :presence => true
+  validates :user_id, :presence => true
+  validates :record_time, :presence => true
+
+  after_initialize :default_values
   
+  private
   
-  # def self.filter_by_params(params)
-  #   models = %w(tribe species camp location)
-  #   models.each do |m|
-  #     if params.include?(m)
-  #       # debugger
-  #       method = "by_" + m.to_s
-  #       value = params.fetch(m.to_sym)
-  #       self.send method, value
-  #     end
-  #   end
-  # end
-  # 
-  # def self.by_species(species_id)
-  #   if species_id
-  #     where(:species_id => species_id)
-  #   else
-  #     all
-  #   end
-  # end
-  # 
-  # def self.by_tribe(tribe_id)
-  #   if tribe_id
-  #     where(:tribe_id => tribe_id)
-  #   else
-  #     all
-  #   end
-  # end
-  # 
-  # def self.by_camp(camp_id)
-  #   if camp_id
-  #     where(:camp_id => camp_id)
-  #   else
-  #     all
-  #   end
-  # end
-  # 
-  # def self.by_time(filter_time)
-  #   if filter_time
-  #     where("record_time > ?", (Date.today - filter_time.to_i))
-  #   else
-  #     all
-  #   end
-  # end
+  def default_values
+    self.record_time ||= Time.now
+    self.submission_point ||= "HQ"
+    self.time_window_hr ||= 4
+  end
 
 end
