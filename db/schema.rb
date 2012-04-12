64fe9c94852855c0414a78693bfb81bd1938ce49
Jon Smith <jonathan@impacti.org>
2012-04-12 23:03:52 +0100

    updated schema for cartodb_id for camps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406165146) do
+ActiveRecord::Schema.define(:version => 20120412213704) do
 
   create_table "camps", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120406165146) do
     t.integer  "location_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "cartodb_id"
   end
 
   create_table "companies", :force => true do |t|
