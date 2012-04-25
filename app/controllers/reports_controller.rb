b01c1683df520a22866d6e208f89257346837121
Jon Smith <jonathan@impacti.org>
2012-04-25 22:35:52 +0100

    cleaned reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,8 @@
 class ReportsController < ApplicationController
   def index
+    @q = Sighting.search(params[:q])
+    # @sightings = @q.result(:distinct => true).paginate(:per_page => 5, :page => params[:page])
+    @sightings = Sighting.find(:all, :limit => 20)
     
   end
 end
