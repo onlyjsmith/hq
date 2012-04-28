3cb208f926bc978ba7e57468c6b18a3ca0e65e41
Jon Smith <jonathan@impacti.org>
2012-04-28 17:10:36 +0100

    assets initialise on precompile false

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,8 @@ module Hq
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Added for Heroku deploy
+    config.assets.initialize_on_precompile = false
   end
 end
